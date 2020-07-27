a7d721541a137c9bd9de7db38bf76ad17a9130ce
Kauã Lopes Sousa <kaualopes562@gmail.com>
2020-07-27 20:27:11 -0300

    :package: sessão orçamento  e rodapé

--- script.js
+++ script.js
@@ -2,3 +2,29 @@
  document.querySelector(".hamburguer").addEventListener("click", function(){
     var sidebar = document.querySelector(".container").classList.toggle("show-menu");
 });
+
+
+
+document.querySelector("#qtde").addEventListener("change", atualizarPreco)
+document.querySelector("#js").addEventListener("change", atualizarPreco)
+document.querySelector("#win10-sim").addEventListener("change", atualizarPreco)
+document.querySelector("#win10-não").addEventListener("change", atualizarPreco)
+document.querySelector("#prazo").addEventListener("change", function () {
+    const prazo = document.querySelector("#prazo").value
+    document.querySelector("label[for=prazo]").innerHTML = `Prazo: ${prazo} semanas`
+    atualizarPreco()
+})
+function atualizarPreco(){
+    
+    const qtde = document.querySelector("#qtde").value
+    const temJS = document.querySelector("#js").checked
+    const incluiLayout = document.querySelector("#win10-sim").checked
+    const prazo = document.querySelector("#prazo").value
+    let preco = qtde * 2500;
+    if (temJS) preco *= 1.1
+    if (incluiLayout) preco += 500
+    let taxaUrgencia = 1 - prazo*0.1;
+    preco *= 1 + taxaUrgencia
+    document.querySelector("#preco").innerHTML = `R$ ${preco.toFixed(2)}`
+}
+
